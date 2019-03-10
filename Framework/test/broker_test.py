e8ccd6bab40609fdea1eacd9f04aa775b5676ad1
Antonia Reiter <antonia.reiter@freenet.de>
2019-03-10 12:04:03 +0100

    Process kill asserts added

--- Framework/test/broker_test.py
+++ Framework/test/broker_test.py
@@ -2,37 +2,41 @@ import unittest
 from test import support
 from subprocess import Popen, PIPE
 import sys, os
-
+import subprocess as sp
 
 pathname = os.path.dirname(sys.argv[0])        
 curpath=os.path.abspath(pathname)
+broker_path=os.path.join(curpath,"..\MQTTBroker\mosquitto.exe")
 
-print('full path =',curpath )       
-
-class MyTestCase1(unittest.TestCase):
+class TestMQTTBroker(unittest.TestCase):
 
     # Only use setUp() and tearDown() if necessary
 
     def setUp(self):
         print("\nStart mosquitto broker")
-        broker_path=os.path.join(curpath,"..\MQTTBroker\mosquitto.exe")
-        print(broker_path)
-        self.p = Popen(broker_path, shell=True, stdout = PIPE)
+        assert os.path.isfile(broker_path) == True
+
+
+        self.p = sp.Popen(broker_path, stdout = sp.PIPE)
+        streamdata = self.p.communicate()[0]
+        assert self.p.returncode == 1
 
     def tearDown(self):
         print("\nKill mosquitto broker")
         self.p.kill()
+        #streamdata = self.p.communicate()[0]
+        assert self.p.returncode == 1
 
     def test_feature_one(self):
         # Test feature one.
-       print("... testing code ...")
+        print("... testing code ...")
 
     def test_feature_two(self):
         # Test feature two.
         print("... testing code ...")
 
 def test_main():
-    support.run_unittest(MyTestCase1)
+    support.run_unittest(TestMQTTBroker)
 
 if __name__ == '__main__':
     test_main()
