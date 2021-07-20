0aff7e66f2b6e138e1b56a567e974ea6b549eb32
Susanna Maria Hepp <susanna@olsoni.de>
2021-07-20 17:14:07 +0200

    small restructuring of main

--- Teams/TeamSusanna/maze_main.py
+++ Teams/TeamSusanna/maze_main.py
@@ -1,14 +1,24 @@
-from tkinter import StringVar, Label, Canvas, Tk, IntVar
-import SusannaAStarAlgo
-import sys
+"""Maze solver algorithm test script 
+
+This script allows the user to load an maze from text, execute the maze solver and 
+visualize the maze and solution path using tkinter
+"""
+
+from tkinter import Tk
+import argparse
 import threading
 import os
-
+import sys
 sys.path.append("../..")
+import SusannaAStarAlgo
+
 import Framework.Visualizer.maze_visualize
 
 def on_closing():
+    """ Callback function if Visualizer Window is closed
+    """
     os._exit(0)
+
 
 apptk = Tk()
 apptk.protocol("WM_DELETE_WINDOW", on_closing)
@@ -19,36 +29,49 @@ apptk.resizable(True, True)
 alg = SusannaAStarAlgo.SusannaAStarAlgo()
 vis = Framework.Visualizer.maze_visualize.MazeVisualizer(apptk)
 
+def main():
+    """[summary]
+    """
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        'input_file',
+        type=str,
+        help="The maze as text file"
+    )
+    args = parser.parse_args()
+    
+    if not alg.loadMaze(args.input_file):
+        exit(1)
 
-if not alg.loadMaze("ascii_image.txt"):
-    exit(1)
+    print("[TeamTemplateAlgo]: loaded maze\n", alg.grid)
 
-print("[TeamTemplateAlgo]: loaded maze\n", alg.grid)
+    vis.clearMaze()
+    vis.setDimRowsCmd(alg.dimRows)
+    vis.setDimColsCmd(alg.dimCols)
+    vis.startMaze(vis.columns, vis.rows)
+    vis.setStartCol(alg.startCol)
+    vis.setEndCol(alg.endCol)
+    vis.setStartRow(alg.startRow)
+    vis.setEndRow(alg.endRow)
+    vis.initialize_grid(False)
 
-vis.clearMaze()
-vis.setDimRowsCmd(alg.dimRows)
-vis.setDimColsCmd(alg.dimCols)
-vis.startMaze(vis.columns, vis.rows)
-vis.setStartCol(alg.startCol)
-vis.setEndCol(alg.endCol)
-vis.setStartRow(alg.startRow)
-vis.setEndRow(alg.endRow)
-vis.initialize_grid(False)
+    for row in range(alg.dimRows):
+        for col in range(alg.dimCols):
+            if alg.grid[row][col]==alg.OBSTACLE:
+                vis.setBlocked(row,col)
 
-for row in range(alg.dimRows):
-    for col in range(alg.dimCols):
-        if alg.grid[row][col]==alg.OBSTACLE:
-            vis.setBlocked(row,col)
-
-vis.endMaze()
+    vis.endMaze()
+    t = threading.Thread(target=mainloop)
+    t.start()
+    apptk.mainloop()
 
 def mainloop():
+    """[summary]
+    """
     solutionString = alg.solveMaze()
     print("[TeamTemplateAlgo]: Result of solving maze: ", solutionString)
     for move in alg.resultpath:
         vis.addSolutionStep(move[0],move[1])    
 
-t = threading.Thread(target=mainloop)
-t.start()
-
-apptk.mainloop()
+if __name__ == "__main__":
+    main()
