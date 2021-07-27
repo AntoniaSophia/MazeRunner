29f8086c83d2534ab3ba7c844d4ffd5c263fdade
Susanna Maria Hepp <susanna@olsoni.de>
2021-07-27 20:32:26 +0200

    wrong order of imports

--- Teams/TeamSusanna/tests/test_solver.py
+++ Teams/TeamSusanna/tests/test_solver.py
@@ -1,11 +1,11 @@
 
-from SusannaAStarAlgo import SusannaAStarAlgo
 import unittest
 import sys
 import os
 import pytest
 
 sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
+from SusannaAStarAlgo import SusannaAStarAlgo
 
 mg = SusannaAStarAlgo()
 
